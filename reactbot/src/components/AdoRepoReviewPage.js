3de8351c2af92a0623b543a7d66bf27f0a632ab5
Chetan4458 <111554115+Chetan4458@users.noreply.github.com>
2024-12-13 12:37:26 +0530

    Update AdoRepoReviewPage.js

--- reactbot/src/components/AdoRepoReviewPage.js
+++ reactbot/src/components/AdoRepoReviewPage.js
@@ -210,9 +210,7 @@ const AdoRepoReview = ({ orgFile, adoauthcode }) => {
     formData.append("org_file", orgFile);
 
     try {
-      const response = await axios.post("/api/review/ado-repo/", formData,{
-  timeout: 0, // Disable timeout (wait indefinitely)
-});
+      const response = await axios.post("/api/review/ado-repo/", formData);
       if (response.data && Array.isArray(response.data.reviews_data)) {
         setReviewResults(response.data.reviews_data);
         console.log(reviewResults);
